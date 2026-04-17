e834c67adc21327f047085400fdefcd73cf12ff1
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-17 23:36:32 +0200

    Update bubbles.js

--- js/bubbles.js
+++ js/bubbles.js
@@ -2,20 +2,29 @@ let bubbles = [];
 
 function shootBubble(){
   bubbles.push({
-    x:player.x,
-    y:player.y,
-    vx:player.dir*2,
-    vy:-0.5,
-    w:6,h:6
+    x: player.x + player.w/2,
+    y: player.y + player.h/2,
+    vx: player.dir * 2,
+    vy: 0, // 🔥 plus de diagonale
+    w: 6,
+    h: 6,
+    life: 0
   });
 }
 
 function updateBubbles(){
   bubbles.forEach(b=>{
-    b.x += b.vx;
-    b.y += b.vy;
-    b.vy -= 0.02;
+    b.life++;
 
+    // déplacement horizontal au début
+    if(b.life < 20){
+      b.x += b.vx;
+    } else {
+      // puis montée verticale
+      b.y -= 1;
+    }
+
+    // capture ennemi
     enemies.forEach(e=>{
       if(!e.trapped && hit(b,e)){
         e.trapped = true;
