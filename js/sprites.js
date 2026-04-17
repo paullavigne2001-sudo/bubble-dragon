fa5ea33528fe4be53afa042ee1d88a2324a0d9f7
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-17 22:46:59 +0200

    Create sprites.js

---
+++ js/sprites.js
@@ -0,0 +1,34 @@
+const SPRITES = {
+  player: [
+    "  XX  ",
+    " XXXX ",
+    "XXXXXX",
+    "XX  XX",
+    "XXXXXX",
+    "X XX X",
+    "X    X",
+    "      "
+  ],
+
+  enemy: [
+    " XXXX ",
+    "XXXXXX",
+    "XX  XX",
+    "XXXXXX",
+    " XXXX ",
+    "X XX X",
+    "      ",
+    "      "
+  ],
+
+  bubble: [
+    " XXXX ",
+    "X    X",
+    "X    X",
+    " XXXX ",
+    "      ",
+    "      ",
+    "      ",
+    "      "
+  ]
+};
