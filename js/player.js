6fba7d5ef9a6c9836d27f85362c573e979734a8e
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-17 23:01:59 +0200

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,10 +1,16 @@
-let player = {x:20,y:150,vx:0,vy:0,w:8,h:8,onGround:false,dir:1};
+let player = {
+  x:20,y:150,
+  vx:0,vy:0,
+  w:8,h:8,
+  onGround:false,
+  dir:1
+};
 
 function updatePlayer(keys){
   player.vx = 0;
 
-  if(keys["ArrowLeft"]){ player.vx=-2; player.dir=-1;}
-  if(keys["ArrowRight"]){ player.vx=2; player.dir=1;}
+  if(keys["ArrowLeft"]){ player.vx = -2; player.dir = -1; }
+  if(keys["ArrowRight"]){ player.vx = 2; player.dir = 1; }
 
   if(keys["ArrowUp"] && player.onGround){
     player.vy = -5;
@@ -14,15 +20,25 @@ function updatePlayer(keys){
   player.x += player.vx;
   player.y += player.vy;
 
-  player.onGround=false;
+  player.onGround = false;
 
   platforms.forEach(p=>{
-    if(hit(player,p) && player.vy>0){
-      player.y = p.y-player.h;
+    if(hit(player,p) && player.vy > 0){
+      player.y = p.y - player.h;
       player.vy = 0;
-      player.onGround=true;
+      player.onGround = true;
     }
   });
+
+  // LIMITES ÉCRAN
+  if(player.x < 0) player.x = 0;
+  if(player.x > 160 - player.w) player.x = 160 - player.w;
+
+  if(player.y > 200 - player.h){
+    player.y = 200 - player.h;
+    player.vy = 0;
+    player.onGround = true;
+  }
 
   if(keys[" "]){
     shootBubble();
