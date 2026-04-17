a77173fa79023f7ac0fb4192eb523a34dfe11bcd
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-17 23:02:27 +0200

    Update enemies.js

--- js/enemies.js
+++ js/enemies.js
@@ -2,26 +2,53 @@ let enemies = [];
 
 function updateEnemies(){
   enemies.forEach(e=>{
+
     if(e.trapped){
       e.y -= 0.5;
-      if(hit(e,player)) e.dead=true;
+      if(hit(e,player)) e.dead = true;
       return;
     }
 
+    // vitesse stable
+    if(!e.vx) e.vx = 1;
+
     e.x += e.vx;
-    e.vy += 0.3;
+
+    // gravité douce
+    e.vy += 0.2;
     e.y += e.vy;
 
+    let onGround = false;
+
     platforms.forEach(p=>{
-      if(hit(e,p) && e.vy>0){
-        e.y = p.y-e.h;
+      if(hit(e,p) && e.vy > 0){
+        e.y = p.y - e.h;
         e.vy = 0;
+        onGround = true;
       }
     });
 
-    if(e.x<0 || e.x>150) e.vx *= -1;
+    // rebond bords
+    if(e.x <= 0){
+      e.x = 0;
+      e.vx = 1;
+    }
 
-    if(hit(e,player)) resetLevel();
+    if(e.x >= 160 - e.w){
+      e.x = 160 - e.w;
+      e.vx = -1;
+    }
+
+    // limite bas écran
+    if(e.y > 200 - e.h){
+      e.y = 200 - e.h;
+      e.vy = 0;
+    }
+
+    // collision joueur
+    if(hit(e,player)){
+      resetLevel();
+    }
   });
 
   enemies = enemies.filter(e=>!e.dead);
