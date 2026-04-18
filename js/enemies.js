409ee0713b54fb902e5fe504758da0eff367e64d
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-18 02:02:54 +0200

    Update enemies.js

--- js/enemies.js
+++ js/enemies.js
@@ -18,8 +18,15 @@ window.updateEnemies = function(){
       }
     });
 
-    if(e.x <= 0){ e.x = 0; e.vx = 1; }
-    if(e.x >= 160 - e.w){ e.x = 160 - e.w; e.vx = -1; }
+    // limites horizontales (interieur des murs : 16px a gauche, 144px a droite)
+    if(e.x <= 16){ e.x = 16; e.vx = 1; }
+    if(e.x >= 144 - e.w){ e.x = 144 - e.w; e.vx = -1; }
+
+    // limite basse : ne pas sortir de l'ecran
+    if(e.y >= 200 - e.h){
+      e.y = 200 - e.h;
+      e.vy = 0;
+    }
 
     if(hit(e,player)){
       resetLevel();
