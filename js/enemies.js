be4176eee1f1d06180ca76c997240c1429bc4be3
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-18 02:33:09 +0200

    Update enemies.js

--- js/enemies.js
+++ js/enemies.js
@@ -7,25 +7,39 @@ window.updateEnemies = function(){
 
     if(!e.vx) e.vx = 1;
 
+    // init jump timer
+    if(!e.jumpTimer) e.jumpTimer = Math.floor(Math.random() * 120) + 60;
+
+    e.jumpTimer--;
+    if(e.jumpTimer <= 0 && e.onGround){
+      e.vy = -3.5;
+      e.onGround = false;
+      e.jumpTimer = Math.floor(Math.random() * 120) + 60;
+    }
+
     e.x += e.vx;
     e.vy += 0.2;
     e.y += e.vy;
+
+    e.onGround = false;
 
     platforms.forEach(p=>{
       if(hit(e,p) && e.vy > 0){
         e.y = p.y - e.h;
         e.vy = 0;
+        e.onGround = true;
       }
     });
 
-    // limites horizontales (interieur des murs : 16px a gauche, 144px a droite)
+    // limites horizontales (interieur des murs)
     if(e.x <= 16){ e.x = 16; e.vx = 1; }
     if(e.x >= 144 - e.w){ e.x = 144 - e.w; e.vx = -1; }
 
-    // limite basse : ne pas sortir de l'ecran
+    // limite basse
     if(e.y >= 192 - e.h){
       e.y = 192 - e.h;
       e.vy = 0;
+      e.onGround = true;
     }
 
     if(hit(e,player)){
